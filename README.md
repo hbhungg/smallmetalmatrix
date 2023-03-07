4d7fff5a879b4da209bcb95685d96a24709a7fab
hung <bahung1tt@gmail.com>
2023-03-08 01:05:28 +1100

    device and buffer

---
+++ README.md
@@ -0,0 +1,13 @@
+# smallmatrix
+
+## Overview
+A simple matrix computation library using M1 GPU with [Metal API C++](https://github.com/bkaradzic/metal-cpp).
+
+This is for [smalltensor](https://github.com/hbhungg/smalltensor) GPU backend.
+
+## Why not Swift?
+Cause I want to learn C++. (Not that Swift is not a good language)
+
+## Why not [MetalPerformanceShader](https://developer.apple.com/documentation/metalperformanceshaders) (MPS)?
+That would kinda defeat the purpose of building your own matrix computation library.
+Plus, this is a learning projects. We are trying to build stuff from the ground up.
--- src/smallmatrix.cpp
+++ src/smallmatrix.cpp
@@ -1,75 +1,49 @@
-/* #define NS_PRIVATE_IMPLEMENTATION */
-/* #define CA_PRIVATE_IMPLEMENTATION */
-/* #define MTL_PRIVATE_IMPLEMENTATION */
-/* #include <Metal/Metal.hpp> */
-/* #include <Foundation/Foundation.hpp> */
-/* #include <QuartzCore/QuartzCore.hpp> */
-#include <iostream>
+#define NS_PRIVATE_IMPLEMENTATION
+#define CA_PRIVATE_IMPLEMENTATION
+#define MTL_PRIVATE_IMPLEMENTATION
+#include <Metal/Metal.hpp>
+#include <Foundation/Foundation.hpp>
+#include <QuartzCore/QuartzCore.hpp>
 #include <stdlib.h>
+#include <vector>
+#include <iostream>
 #include <time.h>
-#include <chrono>
 
-#define M 10248
-#define N 10248
+#define N 10
+// Create a Metal device
+MTL::Device *device = MTL::CreateSystemDefaultDevice();
+MTL::Buffer *mBufferA = device->newBuffer(N, MTL::ResourceStorageModeShared);
+MTL::Buffer *mBufferB = device->newBuffer(N, MTL::ResourceStorageModeShared);
+MTL::Buffer *mBufferResult = device->newBuffer(N, MTL::ResourceStorageModeShared);
 
-// Declare
-float matrix1_cpu[M][N];
-float matrix2_cpu[M][N];
-float matrix_ret_cpu[M][N];
 
-// Print matrix
-template<size_t m, size_t n>
-std::ostream& operator<<(std::ostream& os, const float (&matrix)[m][n]) {
-  for (int i=0;i<m;i++) {
-    for (int j=0;j<n;j++) {
-      std::cout << matrix[i][j] << " ";
-    }
-    std::cout << std::endl;
-  }
-  return os;
-}
-
-template<size_t m, size_t n>
-void init_random(float (&matrix)[m][n], float LOW=0.0, float HIGH=1.0) {
-  for (int i=0;i<m;i++) {
-    for (int j=0;j<n;j++) {
-      float num = LOW + static_cast<float>(rand()) / (static_cast<float>(RAND_MAX/(HIGH-LOW)));
-      matrix[i][j] = num;
-    }
+void randn(MTL::Buffer *buffer) {
+  float *dataPtr = (float* )buffer->contents();
+  for (unsigned long index = 0; index < N; index++) {
+    dataPtr[index] = (float)rand() / (float)(RAND_MAX);
   }
 }
 
-template<size_t m, size_t n>
-void add_matrix_cpu(float (&m1)[m][n], float (&m2)[m][n], float (&ret)[m][n]) {
-  for (int i=0;i<m;i++) {
-    for (int j=0;j<n;j++) {
-      ret[i][j] = m1[i][j] + m2[i][j];
-    }
+void printBuffer(MTL::Buffer *buffer) {
+  float *dataPtr = (float* )buffer->contents();
+  for (unsigned long index = 0; index < N; index++) {
+    printf("%f ", dataPtr[index]);
   }
 }
+
+class Matrix {
+  public:
+    MTL::Buffer *buffer;
+    std::vector<int> strides;
+    std::vector<int> shapes;
+    Matrix(MTL::Buffer *b) { 
+      buffer = b; 
+    }
+};
 
 int main() {
-  /* srand(time(NULL)); */
-  // Create a Metal device
-  /* id<MTL::Device> device = MTL::CreateSystemDefaultDevice(); */
-
-  /* MTL::Buffer *pBuffer = device->newBuffer( */
-
-  // Init
-  init_random(matrix1_cpu, 0, 10);
-  init_random(matrix2_cpu, 0, 10);
-
-  /* std::cout << matrix1_cpu << std::endl; */
-  /* std::cout << matrix2_cpu << std::endl; */
-  auto start = std::chrono::high_resolution_clock::now();
-  add_matrix_cpu(matrix1_cpu, matrix2_cpu, matrix_ret_cpu);
-  auto stop = std::chrono::high_resolution_clock::now();
-  auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-  float in_second = static_cast<float>(duration.count())/1e6;
-  float flops = M*N/in_second/1e12;
-  std::cout << duration.count() << "ms" << std::endl;
-  std::cout << in_second << "s" << std::endl;
-  std::cout << "Single thread TFLOPS: " << flops << std::endl;
-  /* std::cout << matrix_ret_cpu; */
+  srand(time(NULL));
+  randn(mBufferA);
+  printBuffer(mBufferA);
+  /* randn(mBufferB); */
 }
-
